273119e4b0a97bed26c708e32aaec06e123750fb
harsh gajera <harshgajera202@gmail.com>
2023-04-16 10:37:45 +0530

    ALL commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
+// import Article from './components/Articals/Article'
+// import Brand from './components/brand/Brand'
+// import CTA from './components/cta/CTA'
+// import Feature from './components/feature/Feature'
+// import Navbar from './components/navbar/Navbar'
+
+import {Article, Brand, CTA, Feature, Navbar} from './components'
+import {Blog, Features, Footer, Header, Posibility, WhatGPT3} from './containers'
+import './app.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Posibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
